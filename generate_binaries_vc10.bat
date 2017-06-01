1a9095a52fbd08a4b4433d403a9164f062d51bc5
Amaury Bréhéret <abreheret@gmail.com>
2017-06-01 18:08:31 +0200

    add auto generation for boost cmake package

--- generate_binaries_vc10.bat
+++ generate_binaries_vc10.bat
@@ -1,17 +1,16 @@
 mkdir buildx64
 cd buildx64
 cmake -G "Visual Studio 10 Win64" ..
-cmake --build . --config Release --target ALL_BUILD
-REM cmake --build . --config Release --target RUN_TESTS
-REM cmake --build . --config Debug --target install
+cmake --build . --config Release --target install
+cmake --build . --config Debug --target install
 cd ..
 
-REM mkdir buildx86
-REM cd buildx86
-REM cmake -G "Visual Studio 10" .
-REM cmake --build . --config Release --target install
-REM cmake --build . --config Debug --target install
-REM cd ..
+mkdir buildx86
+cd buildx86
+cmake -G "Visual Studio 10" .
+cmake --build . --config Release --target install
+cmake --build . --config Debug --target install
+cd ..
 
 REM rd /s /q buildx64
 REM rd /s /q buildx86
